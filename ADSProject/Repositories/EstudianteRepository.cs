258ab7707fbdbd33f231d8f542eb09b70871b39c
SalvadorDeAsis <dm18i04001@usonsonate.edu.sv>
2024-05-24 17:42:43 -0600

    Se ha greado un nuevo proyecto Guia 6 y Guia 5

--- ADSProject/Repositories/EstudianteRepository.cs
+++ ADSProject/Repositories/EstudianteRepository.cs
@@ -1,20 +1,25 @@
-﻿using ADSProject.Interfaces;
+﻿using ADSProject.DB;
+using ADSProject.Interfaces;
 using ADSProject.Models;
 
 namespace ADSProject.Repositories
 {
     public class EstudianteRepository : IEstudiante
     {
-       
-        private List<Estudiante> lstEstudiantes = new List<Estudiante>()
-        {
-            new Estudiante
-            {   IdEstudiante = 1,NombresEstudiante = "Juan Carlos",
-                ApellidosEstudiante="Perez Sosa",CodigoEstudiante="PS24I04002",
-                CorreoEstudiante="PS24I04002@usonsonate.edu.sv"
-            }
-        };
+        /*
+         private List<Estudiante> lstEstudiantes = new List<Estudiante>()
+         {
+             new Estudiante
+             {   IdEstudiante = 1,NombresEstudiante = "Juan Carlos",
+                 ApellidosEstudiante="Perez Sosa",CodigoEstudiante="PS24I04002",
+                 CorreoEstudiante="PS24I04002@usonsonate.edu.sv"
+             }
 
+         };
+
+          */
+
+        private readonly ApplicationDbContext applicationDbContext;
 
         public int AgregarEstudiante(Estudiante estudiante)
         {
@@ -22,12 +27,15 @@ namespace ADSProject.Repositories
             {
                 //Validar si existen datos en la lista, de ser asi, tomaremos el ultimo ID
                 //Y lo incrementamos en una unidad
-                if (lstEstudiantes.Count > 0)
+               /* if (lstEstudiantes.Count > 0)
                 {
                     estudiante.IdEstudiante = lstEstudiantes.Last().IdEstudiante + 1;
                 }
                 lstEstudiantes.Add(estudiante);
+                */
 
+                applicationDbContext.Estudiantes.Add(estudiante);
+                applicationDbContext.SaveChanges();
                 return estudiante.IdEstudiante;
 
             }
@@ -44,9 +52,13 @@ namespace ADSProject.Repositories
             try
             {
                 //Obtenemos el indice del objeto para actualizar
-                int indice = lstEstudiantes.FindIndex(tmp => tmp.IdEstudiante == idEstudiante);
+               // int indice = lstEstudiantes.FindIndex(tmp => tmp.IdEstudiante == idEstudiante);
                 //Procedemos con la actualizacion
-                lstEstudiantes[indice] = estudiante;
+               // lstEstudiantes[indice] = estudiante;
+
+                var item = applicationDbContext.Estudiantes.SingleOrDefault(x => x.IdEstudiante == idEstudiante);
+                applicationDbContext.Entry(item).CurrentValues.SetValues(estudiante);
+                applicationDbContext.SaveChanges();
 
                 return idEstudiante;
 
@@ -64,10 +76,12 @@ namespace ADSProject.Repositories
             try
             {
                 //Obtenemos el indice del objeto a eliminar
-                int indice = lstEstudiantes.FindIndex(tmp => tmp.IdEstudiante == IdEstudiante);
+                //int indice = lstEstudiantes.FindIndex(tmp => tmp.IdEstudiante == IdEstudiante);
                 //Procedemos a eliminar el registro
-                lstEstudiantes.RemoveAt(indice);
-
+                //lstEstudiantes.RemoveAt(indice);
+                var item = applicationDbContext.Estudiantes.SingleOrDefault(x => x.IdEstudiante == IdEstudiante);
+                applicationDbContext.Estudiantes.Remove(item);
+                applicationDbContext.SaveChanges();
                 return true;
             }
             catch (Exception)
@@ -80,8 +94,8 @@ namespace ADSProject.Repositories
             try
             {
                 //Buscamos asignamos el objeto Estudiante
-                Estudiante estudiante = lstEstudiantes.FirstOrDefault(tmp => tmp.IdEstudiante == IdEstudiante);
-
+                //Estudiante estudiante = lstEstudiantes.FirstOrDefault(tmp => tmp.IdEstudiante == IdEstudiante);
+                var estudiante = applicationDbContext.Estudiantes.SingleOrDefault(x => IdEstudiante == IdEstudiante);
                 return estudiante;
             }
             catch(Exception)
@@ -95,7 +109,9 @@ namespace ADSProject.Repositories
         {
             try
             {
-                return lstEstudiantes;
+              //  return lstEstudiantes;
+              return applicationDbContext.Estudiantes.ToList();
+
             }
             catch (Exception)
             {
--- ADSProject/obj/Debug/net8.0/ADSProject.AssemblyInfo.cs
+++ ADSProject/obj/Debug/net8.0/ADSProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADSProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bde2a1d8b2a5c098919f4f0ac271b7de0a0fc0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ce725f43c35f17cfb853ef1ad8dd05f76fab73")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADSProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADSProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
